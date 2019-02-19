51e2e06534a3f020a72c3dd34d3f673d6f55fd7c
PJasiczek <jasiu1047@gmail.com>
2019-02-19 01:38:53 +0100

    Login/Logout system in website page code

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['initiate'])){
+        session_regenerate_id();
+        $new_session_id = session_id();
+        session_write_close();
+        session_id($new_session_id);
+        session_start();
+        $_SESSION['initiate'] = 1;
+    }
+
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Piotr Jasiczek"/>
+        <meta name="robots" content="index,follow"/>
+        <meta name="description" content=""/>
+        <title>Zaloguj się</title>
+        <meta name="keywords" content="logowanie"/>
+        <meta name="copyright" content="Piotr Jasiczek"/>
+        <link rel="stylesheet" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="container_main">
+            <div class="container_login">
+                <div class="login">
+                <form action="index.php" method="post" enctype="multipart/form-data">
+                    <div>
+                        <div>
+                           <input type="text" value="Username" name="login" maxlength="8" size="15"/>
+                        </div>
+                        <div>
+                           <input type="password" value="Password" name="password" maxlength="15" size="15" />
+                        </div>
+                        <div>
+                            <input type="submit" value="Zaloguj się" />
+                        </div>
+                    </div>
+                </form>
+                    <?php
+                        if(isset($_GET['action']) && ($_GET['action'] == "logout")){                  
+
+                            $_SESSION['online'] = 0;
+                            session_destroy();
+                            echo "<div style=\" position: absolute; width: 200px; margin-top: 15%; margin: 0 auto; padding-top: 20px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 10px; color: red;\"><b> Nastąpiło poprawne wylogowanie! </b></div>";                 
+                        }
+                        if(($_SESSION['online'] == 1) && ($_SESSION['specification_info'] != $_SERVER['HTTP_USER_AGENT'])){
+
+                            $_SESSION['online'] = 0;
+                            session_destroy(); 
+                            echo "<div style=\" position: absolute; width: 200px; margin-top: 15%; margin: 0 auto; padding-top: 20px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 10px; color: red;\"><b> Prosimy o ponowne wprowadzenie nazwy użytkownika i hasła. </b></div>";
+                        }
+                        if(($_SESSION['online'] == 1) && ((time() - $_SESSION['time']) > 300)){
+
+                            $_SESSION['online'] = 0;
+                            session_destroy();
+                            echo "<div style=\" position: absolute; width: 200px; margin-top: 15%; margin: 0 auto; padding-top: 20px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 10px; color: red;\"><b> Nastąpiło wylogowanie w wyniku przekroczenia czasu trwania sesji. </b></div>";
+                        }
+                        
+                        if((isset($_POST['login']) && isset($_POST['password'])) || ($_SESSION['online'] == 1)){
+
+                            if((!empty($_POST['login']) && !empty($_POST['password'])) || ($_SESSION['online'] == 1)){
+
+                                if($_SESSION['online'] == 0){
+                                    $username = filter_var($_POST['login'],FILTER_SANITIZE_STRING);
+                                    $password = filter_var($_POST['password'],FILTER_SANITIZE_STRING);
+                                }
+
+                                if(($username == "username" && $password == "password") || ($_SESSION['online'] == 1)){
+                                    echo "<div style=\" position: absolute; width: 200px; margin-top: 15%; margin: 0 auto; padding-top: 20px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 10px; color: red;\"><b> Nastąpiło poprawne zalogowanie! </br> <a href='index.php?action=logout'> Wyloguj </a> </b></div>";
+                                    $_SESSION['time'] = time();
+                                    $_SESSION['specification_info'] = $_SERVER['HTTP_USER_AGENT'];
+                                    header('Location: login.php');
+                                    exit;
+                                }else{
+                                    echo "<div style=\" position: absolute; width: 200px; margin-top: 15%; margin: 0 auto; padding-top: 20px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 10px; color: red;\"><b> Wprowadzona nazwa użytkownika lub hasło nie należy do konta. Sprawdź swoją nazwę użytkownika lub hasło i spróbuj ponownie. </b></div>";
+                                }
+                        }else{
+                            echo "<div style=\" position: absolute; width: 200px; margin-top: 15%; margin: 0 auto; padding-top: 20px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 10px; color: red;\"><b> Nie została podana nazwa użytkownika lub hasło. Spróbuj ponownie. </b></div>";
+                        }
+                    }              
+                    ?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
---
+++ login.php
@@ -0,0 +1,78 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Piotr Jasiczek"/>
+        <meta name="robots" content="index,follow"/>
+        <meta name="description" content=""/>
+        <title> Wyślij plik </title>
+        <meta name="keywords" content="logowanie"/>
+        <meta name="copyright" content="Piotr Jasiczek"/>
+        <link rel="stylesheet" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="container_upload_file_main">
+            <div class="container_upload_file">
+                <div class="upload_file">
+                    <form action="login.php" method="post" enctype="multipart/form-data">
+                    <div>
+                        <div>
+                            <input type="hidden" name="MAX_FILE_SIZE" value="30720" />
+                        </div>
+                        <div>
+                            <input type="file" name="file" />
+                        </div>
+                         <?php
+                            if(isset($_FILES['file'])){
+                                switch($_FILES['file']['error']){
+
+                                    case 0:
+                                    {
+                                        if($_FILES['file']['type'] == "image/jpeg" || $_FILES['file']['type'] == "image/png" || $_FILES['file']['type'] == "video/mp4"){
+                                                move_uploaded_file($_FILES['file']['tmp_name'], "send_image/".md5(rand()*rand()+rand()).$_FILES['file']['name']);
+                                                echo "<div style=\" position: absolute; width: 700px; margin: 0 auto; padding-top: 20px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 12px; color: red;\"><b> Plik ". $_FILES['file']['name']." został przesłany z powodzeniem </b></div>";
+                                            }else{
+                                                echo "<div style=\" position: absolute; width: 200px; margin: 0 auto; padding-top: 20px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 12px; color: red;\"><b> Nieobsługiwany typ pliku </b></div>";
+                                            }
+                                    }
+                                    break;
+
+                                    case 1:
+                                    {
+                                        echo "<div style=\" position: absolute; width: 200px; margin: 0 auto; padding-top: 10px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 12px; color: red;\"><b> Za duży rozmiar pliku </b></div>";
+                                    }
+                                    break;
+
+                                    case 2:
+                                    {
+                                        echo "<div style=\" position: absolute; width: 200px; margin: 0 auto; padding-top: 10px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 12px; color: red;\"><b> Za duży rozmiar pliku </b></div>";
+                                    }
+                                    break;
+
+                                    case 3:
+                                    {
+                                        echo "<div style=\" position: absolute; width: 200px; margin: 0 auto; padding-top: 10px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 12px; color: red;\"><b> Przesłany plik został tylko częściowo przesłany </b></div>";
+                                    }
+                                    break;
+
+                                    case 4:
+                                    {
+                                        echo "<div style=\" position: absolute; width: 200px; margin: 0 auto; padding-top: 10px; text-align: center; font-family: 'Quicksand', sans-serif; font-size: 12px; color: red;\"><b> Żaden plik nie został przesłany </b></div>";
+                                    }
+                                    break;
+                                }
+                            }
+                        ?>
+                        <div>
+                            <input type="submit" value="Wyślij plik" />
+                        </div>
+                    </div>
+                    </form>
+                </div>
+           </div>
+        </div>
+    </body>
+</html>
